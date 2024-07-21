d06068bb3bd50c94af1a002fb55875e0a728a0ce
Adi023 <adityapandhare050@gmail.com>
2024-07-21 23:01:56 +0530

    Starting New roject Music Player Spotify

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+//NAVIGATION
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator} from "@react-navigation/native-stack"
+
+//SCREENS
+import Home from './screens/Home'
+import Details from './screens/Details'
+
+export type RootStackParamList = {
+    Home :undefined;
+    Details : {productId:string};
+}
+
+const Stack = createNativeStackNavigator<RootStackParamList>()
+
+
+export default function App():JSX.Element {
+  return (
+   
+   <NavigationContainer>
+    <Stack.Navigator initialRouteName='Home'>
+         <Stack.Screen
+         name='Home'
+         component={Home}
+         options={{
+            title:'Trending Products'
+         }}
+         />
+         <Stack.Screen
+         name='Details'
+         component={Details}
+         options={{
+            title:'Products Details'
+         }}
+         />
+    </Stack.Navigator>
+   </NavigationContainer>
+  )
+}
---
+++ src/screens/Home.tsx
@@ -0,0 +1,33 @@
+import { Button, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
+import {RootStackParamList} from '../App'
+
+type HomeProps = NativeStackScreenProps<RootStackParamList,'Home'>
+
+export default function Home({navigation}:HomeProps) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.smallText}>Home Screen</Text>
+      <Button
+      title='Go to etails'
+      onPress={()=>navigation.navigate("Details",{
+        productId:"23"
+      })}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container:{
+        flex:1,
+        alignItems:'center',
+        justifyContent :'center',
+    },
+    smallText:{
+        color:'#000000'
+    }
+
+})
